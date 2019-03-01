7eb8dd10dc3d54684e9e2f0840eb7f0ac0ea6740
kimchoo <34254826+kimchoo@users.noreply.github.com>
2019-03-01 06:37:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-def compile():
-  
-compile()
+from graphics import graphic
+import pygame
+
+__run__ = True
+gr = graphic()
+
+while __run__  == True:
+
+	for event in pygame.event.get():  
+		if event.type == pygame.QUIT: 
+			__run__ = False
+
+	gr.initialize()
+	gr.draw_map()
+
+	gr.update()
